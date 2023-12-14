44a1b50e4c0a934f40ca3591443467e94e4651c0
Hong An Tran <125112230+hongantran3804@users.noreply.github.com>
2023-12-13 20:25:14 -0800

    Update Prediction.py

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split 
-import statsmodels.api as sm
 st.set_page_config("Prediction_Board",layout='wide')
 st.header("Prediction DashBoard")
 df = pd.read_csv("Streamlit_Prediction.csv")
